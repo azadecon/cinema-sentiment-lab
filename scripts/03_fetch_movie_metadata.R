517f969612f65f77b213ab7c8bcdedfa347b9526
azadecon <azadecon@gmail.com>
2025-07-26 01:55:46 +0530

    cleaned director scraper

--- scripts/03_fetch_movie_metadata.R
+++ scripts/03_fetch_movie_metadata.R
@@ -17,10 +17,10 @@ library(httr)
 library(tools)
 
 # Example dataframe
-bolly_dir_collec <- read_csv("./data/clean/bolly_sample_100.csv") %>% select(imdb_id)
+bolly_direc <- read_csv("./data/clean/bolly_sample_100.csv") %>% select(imdb_id)
 
 # Add director column by mapping over imdb ids
-bolly_dir_collec <- bolly_dir_collec %>%
+bolly_direc <- bolly_direc %>%
   mutate(director = map_chr(imdb_id, get_director_name))
 
 
@@ -28,7 +28,7 @@ bolly_dir_collec <- bolly_dir_collec %>%
 metadata_dir = "./data/raw/metadata/"
 if (!dir.exists(metadata_dir)) dir.create(metadata_dir, recursive = TRUE)
 
-write_csv(bolly_dir_collec, paste0(metadata_dir, "/bolly_dir_collec.csv"))
+write_csv(bolly_direc, paste0(metadata_dir, "/bolly_direc.csv"))
 
 
 #########################################################################################################
